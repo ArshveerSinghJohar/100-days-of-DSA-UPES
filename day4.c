c2eb1a6be269f3e086f893e6d62a5b979586d0c0
ArshveerSinghJohar <arshveer26j@gmail.com>
2026-02-04 17:02:44 +0530

    Add files via upload

---
+++ day4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int left = 0, right = n - 1;
+    int temp;
+
+    // Two-pointer approach
+    while (left < right) {
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    printf("Reversed array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

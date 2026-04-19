99cd30565320323f2800eb8d6661038545cb939d
ArshveerSinghJohar <arshveer26j@gmail.com>
2026-04-19 10:40:34 +0530

    Add files via upload

---
+++ day 78.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int n, m;
+int graph[MAX][MAX];
+
+int minKey(int key[], int visited[]) {
+    int min = INT_MAX, min_index;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i] && key[i] < min) {
+            min = key[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+int primMST() {
+    int parent[MAX];
+    int key[MAX];
+    int visited[MAX];
+
+    // Initialization
+    for (int i = 1; i <= n; i++) {
+        key[i] = INT_MAX;
+        visited[i] = 0;
+    }
+
+    key[1] = 0;   // start from node 1
+    parent[1] = -1;
+
+    int totalWeight = 0;
+
+    for (int count = 1; count <= n; count++) {
+        int u = minKey(key, visited);
+        visited[u] = 1;
+
+        totalWeight += key[u];
+
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+                parent[v] = u;
+            }
+        }
+    }
+
+    return totalWeight;
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    // initialize graph
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            graph[i][j] = 0;
+
+    int u, v, w;
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w;  // undirected
+    }
+
+    int result = primMST();
+    printf("%d\n", result);
+
+    return 0;
+}
